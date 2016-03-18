72f40fdf4f2801fd223767fd3e92e1dd1a67577e
cat2151 <fct_2004@mail.goo.ne.jp>
2016-03-18 21:43:47 +0900

    ofgs2のmmlcと、nsd.libのnsc用のMML出力機能を追加した

--- services.js
+++ services.js
@@ -41,18 +41,38 @@ function() {
     var inMmls = sionmml.split(";");
     var outMml = "";
     var ch = 0;
+    outMml += genHead();
     angular.forEach(inMmls, function(inMml) {
       outMml += gen1ch(inMml, ch);
       ch++;
     });
+    outMml += genTail();
     return outMml;
+    function genHead() {
+      if (compiler == "nsc") { // nsd.lib [イメージ] "#Code "../../bin/nsd.bin" bgm(0){ TR1 ceg }
+        return "#Code " + '"' +"../../bin/nsd.bin" + '"' + " bgm(0) {\n";
+      }
+      return "";
+    }
     function gen1ch(inMml, ch) {
       if (compiler == "mckc") { // [イメージ] "A v15cde"
         if (ch < 2) return getABC(ch) + " " + "v15" + inMml + "\n"; // AB : pulse
         if (ch == 2) return getABC(ch) + " " + inMml + "\n"; // C : triangle
       }
+      if (compiler == "mmlc") { // ofgs2 [イメージ] "#TRACK 0,1\nl4cde\n;\n"
+        return "#TRACK " + ch + "," + (ch + 1) + "\nl4" + inMml + "\n;\n";
+      }
+      if (compiler == "nsc") { // nsd.lib [イメージ] "#Code "../../bin/nsd.bin" bgm(0){ TR1 ceg }
+        return "TR" + (ch + 1) + "\n" + inMml + "\n";
+      }
       if (compiler == "note.x") { // [イメージ] "A @0v15cde"
         return getABC(ch) + " " + "@0v15" + inMml + "\n";
+      }
+      return "";
+    }
+    function genTail() {
+      if (compiler == "nsc") { // nsd.lib [イメージ] "#Code "../../bin/nsd.bin" bgm(0){ TR1 ceg }
+        return "}\n";
       }
       return "";
     }
